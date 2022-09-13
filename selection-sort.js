b7095595dffbe4f3708d3594e5d50b8e9ce9dec9
Trent Krause <105151007@edu.cbe.ab.ca>
2022-09-13 10:28:55 -0600

    selections sort done

--- selection-sort.js
+++ selection-sort.js
@@ -1,4 +1,4 @@
-function selectionSort(array) {
+function selectionSort(oldArr) {
 	/*
 	 ** Search through the whole array for the smallest element
 	 ** then swap it with the first element.
@@ -6,8 +6,23 @@ function selectionSort(array) {
 	 ** for the smallest, then swap it with the second element.
 	 ** Continue like this until you have only one element left.
 	 */
-
-	return array;
+	let newArr = []
+	for (let rep = 0; rep < oldArr.length+newArr.length; rep++) {
+		let smallestNum
+		let lastUpdate
+		smallestNum = oldArr[0]
+		for (let i = 0; i < oldArr.length; i++) {
+			if(smallestNum > oldArr[i]){
+				smallestNum = oldArr[i]
+				lastUpdate = i
+				console.log(i)
+			}
+		}
+		newArr.push(smallestNum)
+		console.log(newArr)
+		oldArr.splice(lastUpdate, 1)
+	}
+	return newArr;
 }
 
 module.exports = selectionSort;
