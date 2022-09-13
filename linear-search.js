2a8e1cdda5ef504b297b841f56e72aa0d93c868c
Trent Krause <105151007@edu.cbe.ab.ca>
2022-09-13 09:29:32 -0600

    linear search done

--- linear-search.js
+++ linear-search.js
@@ -5,8 +5,13 @@ function linearSearch(array, searchTerm) {
 	 **
 	 ** Return the index of the found element
 	 */
-
-	return null;
+	let where = 0
+	for (let i = 0; i < array.length; i++) {
+		if(array[i] == searchTerm){
+			where  = i
+		}
+	}
+	return where;
 }
 
 module.exports = linearSearch;
