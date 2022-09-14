6d40c9de20e4f75967a77eeb2c767e0104c35ce7
Trent Krause <105151007@edu.cbe.ab.ca>
2022-09-14 11:22:02 -0600

    insertion sort done

--- insertion-sort.js
+++ insertion-sort.js
@@ -4,8 +4,26 @@ function insertionSort(array) {
 	 ** before it over and over as long as the element before it
 	 ** is bigger
 	 */
-
-	return array;
+	if(array[0] != undefined){
+		let newArr = [array[0]]
+		let broken = false
+		for (let rep = 0; rep < array.length-1; rep++) {
+			for (let i = 0; i < newArr.length; i++) {
+				if(array[rep+1] <= newArr[i]){
+					newArr.splice(i, 0, array[rep+1])
+					broken = true
+					break
+				}
+				broken = false
+			}
+			console.log(newArr)
+			if(newArr.length != rep && broken == false){
+				newArr.push(array[rep+1])
+			}
+		}
+		return newArr;
+	}
+	return []
 }
 
 module.exports = insertionSort;
