521db61236671632458e493e27922d9a990a945a
Trent Krause <105151007@edu.cbe.ab.ca>
2022-09-19 11:34:39 -0600

    linear search done

--- linear-search.js
+++ linear-search.js
@@ -1,10 +1,17 @@
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
-function linearSearch(array, searchTerm) {for (let i = 0; i < array.length; i++) {if(array[i] == searchTerm){return i;}}}
+function linearSearch(array, searchTerm) {
+	/*
+	 ** Check each element from start to finish
+	 ** until you find an element that matches the search term
+	 **
+	 ** Return the index of the found element
+	 */
+	let where = 0
+	for (let i = 0; i < array.length; i++) {
+		if(array[i] == searchTerm){
+			where  = i
+		}
+	}
+	return where;
+}
+
 module.exports = linearSearch;
